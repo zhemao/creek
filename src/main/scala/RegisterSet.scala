d5a3ab6fc48671031cf00d53a582ea81821f7c7f
Howard Mao <zhehao.mao@gmail.com>
2014-05-21 17:58:41 -0700

    add scalar value register to scalar memory in register set

--- src/main/scala/RegisterSet.scala
+++ src/main/scala/RegisterSet.scala
@@ -8,6 +8,7 @@ class RegisterSet(depth: Int, bitwidth: Int) extends Module {
         val scalar_writeaddr = UInt(INPUT, 2)
         val scalar_writedata = UInt(INPUT, addr_size)
         val scalar_write = Bool(INPUT)
+        val scalar_value = Bits(OUTPUT, bitwidth)
         val read_reset = Bool(INPUT)
         val write_reset = Bool(INPUT)
         val vector_readdata = Bits(OUTPUT, bitwidth)
@@ -20,6 +21,9 @@ class RegisterSet(depth: Int, bitwidth: Int) extends Module {
     val start = Reg(UInt(width = addr_size))
     val step = Reg(UInt(width = addr_size))
     val count = Reg(UInt(width = addr_size))
+    val scalar = Reg(UInt(width = addr_size))
+
+    io.scalar_value := scalar
 
     val readaddr = Reg(UInt(width = addr_size))
     val readcount = Reg(UInt(width = addr_size))
@@ -33,6 +37,7 @@ class RegisterSet(depth: Int, bitwidth: Int) extends Module {
             is(UInt(0)) { start := io.scalar_writedata }
             is(UInt(1)) { step  := io.scalar_writedata }
             is(UInt(2)) { count := io.scalar_writedata }
+            is(UInt(3)) { scalar := io.scalar_writedata }
         }
     }
 
@@ -70,6 +75,7 @@ class RegisterSetTest(c: RegisterSet) extends Tester(c) {
     val readstart = writestart + writestep
     val readstep = 2 * writestep
     val readcount = writecount / 2
+    val scalar_value = rnd.nextInt(1 << 16)
 
     val writevals = (0 until writecount).map {
         i => rnd.nextInt(1 << 16)
@@ -122,6 +128,15 @@ class RegisterSetTest(c: RegisterSet) extends Tester(c) {
     poke(c.io.scalar_writeaddr, 2)
     poke(c.io.scalar_writedata, readcount)
     step(1)
+
+    poke(c.io.scalar_writeaddr, 3)
+    poke(c.io.scalar_writedata, scalar_value)
+    step(1)
+
+    poke(c.io.scalar_write, 0)
+    step(1)
+
+    expect(c.io.scalar_value, scalar_value)
 
     val readvals = (0 until readcount).map {
         i => writevals(1 + 2 * i)
