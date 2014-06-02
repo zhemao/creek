3e2d51ecf8dd8744260e12bb2ef2ec4267fc379f
Howard Mao <zhehao.mao@gmail.com>
2014-06-01 21:01:28 -0700

    add missing clone methods to UnitBundles

--- chisel/src/main/scala/UnitBundles.scala
+++ chisel/src/main/scala/UnitBundles.scala
@@ -7,15 +7,21 @@ class UnitForwardInput(lanes: Int) extends Bundle {
     val vector_readdata = Bits(width = FloatSize * lanes)
     val busy = Bool()
     val scalar_value = Bits(width = FloatSize)
+
+    override def clone = new UnitForwardInput(lanes).asInstanceOf[this.type]
 }
 
 class UnitBackwardInput(lanes: Int) extends Bundle {
     val read_reset = Bool()
     val vector_read = Bool()
+
+    override def clone = new UnitBackwardInput(lanes).asInstanceOf[this.type]
 }
 
 class UnitBackwardOutput(lanes: Int) extends Bundle {
     val write_reset = Bool()
     val vector_writedata = Bits(width = FloatSize * lanes)
     val vector_write = Bool()
+
+    override def clone = new UnitBackwardOutput(lanes).asInstanceOf[this.type]
 }
