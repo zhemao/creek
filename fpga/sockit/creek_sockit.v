77e73cf5c8bba703812c6f74204de42e7740c2ef
Howard Mao <zhehao.mao@gmail.com>
2014-06-14 12:49:03 -0700

    redo pin assignments

--- fpga/sockit/creek_sockit.v
+++ fpga/sockit/creek_sockit.v
@@ -1,27 +1,27 @@
 `define SYNTHESIS
 
 module creek_sockit (
-    input         OSC_50_B3B,
-    input         RESET_n,
+    input          OSC_50_B3B,
+    input          RESET_n,
 
-    output [14:0] DDR3_A,
-    output [2:0]  DDR3_BA,
-    output        DDR3_CAS_n,
-    output        DDR3_CKE,
-    output        DDR3_CK_n,
-    output        DDR3_CK_p,
-    output        DDR3_CS_n,
-    output [3:0]  DDR3_DM,
+    output [14:0]  DDR3_A,
+    output [2:0]   DDR3_BA,
+    output         DDR3_CAS_n,
+    output         DDR3_CKE,
+    output         DDR3_CK_n,
+    output         DDR3_CK_p,
+    output         DDR3_CS_n,
+    output [3:0]   DDR3_DM,
     inout  [31:0]  DDR3_DQ,
-    inout  [3:0]  DDR3_DQS_n,
-    inout  [3:0]  DDR3_DQS_p,
-    output        DDR3_ODT,
-    output        DDR3_RAS_n,
-    output        DDR3_RESET_n,
-    input         DDR3_RZQ,
-    output        DDR3_WE_n,
+    inout  [3:0]   DDR3_DQS_n,
+    inout  [3:0]   DDR3_DQS_p,
+    output         DDR3_ODT,
+    output         DDR3_RAS_n,
+    output         DDR3_RESET_n,
+    input          DDR3_RZQ,
+    output         DDR3_WE_n,
 
-    output [3:0]  LED
+    output [3:0]   LED
 );
 
 wire [9:0]  instr_readaddr;
